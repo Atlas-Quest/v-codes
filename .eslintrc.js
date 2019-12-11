7d9a147bede6cf2a12e2bc138210412202883472
atlas-quest <vanessa@sveltestudios.com>
2019-12-11 20:31:56 +1030

    nuxt-links

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/essential"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      "tab"
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
+    "linebreak-style": [
+      "error",
+      "unix"
     ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ]
+  }
 };
--- components/header/mobile/Header.vue
+++ components/header/mobile/Header.vue
@@ -1,8 +1,14 @@
 <template>
-<div class="flex justify-end p-3">
-  <div v-show="!active"
+<div class="p-8 bg-white">
+  <div class="flex justify-start">
+    <img src="~/static/Logo.png"
+         alt="logo">
+  </div>
+  <div class="flex justify-end"
        @click="openMenu()">
-    <i-menu class="w-10 h-10"></i-menu>
+    <i-menu v-show="!active"
+            class="w-10 h-10"></i-menu>
+    <slot></slot>
   </div>
   <mobile-nav v-show="active"
               @close="active = false"></mobile-nav>
@@ -10,25 +16,21 @@
 </template>
 
 <script>
-import iMenu from '~/components/icons/Menu';
-import MobileNav from './Nav';
+import iMenu from '~/components/icons/Menu'
+import MobileNav from './Nav'
 
 export default {
-  name: 'MobileHeader',
-  components: { iMenu, MobileNav },
-  data() {
-    return {
-      active: false
-    }
-  },
-  methods: {
-    openMenu() {
-      this.active = !this.active;
-      console.log('THIS.ACTIVE', this.active)
-    }
-  }
+	name: 'MobileHeader',
+	components: { iMenu, MobileNav },
+	data() {
+		return {
+			active: false
+		}
+	},
+	methods: {
+		openMenu() {
+			this.active = !this.active
+		}
+	}
 }
 </script>
-
-<style lang="css" scoped>
-</style>
