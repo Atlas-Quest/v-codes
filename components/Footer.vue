1bb76ad6ebef30b09abcf67659f3bf10cd549bb4
atlas-quest <vanessa@sveltestudios.com>
2019-12-12 11:46:45 +1030

    minor css updates

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,32 +1,26 @@
 <template>
 <div class="flex bg-footer text-white justify-between p-8 flex-col md:flex-row">
-  <div class="md:pl-8 flex flex-wrap justify-center md:no-wrap md:flex md:justify-between">
+  <div class="flex flex-wrap justify-center md:no-wrap md:flex md:justify-between">
     <img class="my-4"
          src="~/static/jsLogo.png"
-         width="auto"
-         height="50px" />
+         width="auto" />
     <img class="my-4"
          src="~/static/netlify.png"
-         width="auto"
-         height="50px" />
+         width="auto" />
     <img class="my-4"
          src="~/static/nuxtLogo.png"
-         width="auto"
-         height="50px" />
+         width="auto" />
     <img class="my-4"
          src="~/static/vueLogo.png"
-         width="auto"
-         height="50px" />
+         width="auto" />
     <img class="my-4"
          src="~/static/tailwind.png"
-         width="auto"
-         height="50px" />
+         width="auto" />
     <img class="my-4"
          src="~/static/nodeLogo.svg"
-         width="auto"
-         height="50px" />
+         width="auto" />
   </div>
-  <div class="flex items-center justify-center md:pr-8 pt-8 md:pt-0 text-center md:text-right">
+  <div class="flex items-center justify-center pt-4 md:pt-0 text-center md:text-right">
     2019 Vanessa Codes
   </div>
 </div>
@@ -40,7 +34,7 @@ export default {
 
 <style scoped>
 img {
-  height: 50px;
+  height: 24px;
   padding-right: 12px;
 }
 </style>
--- pages/Projects.vue
+++ pages/Projects.vue
@@ -71,6 +71,7 @@
       <!-- <div class="swiper-button-prev" slot="button-prev"></div>
         <div class="swiper-button-next" slot="button-next"></div> -->
       <!-- <div class="swiper-scrollbar" slot="scrollbar"></div> -->
+
       <div class="swiper-pagination"
            slot="pagination"></div>
     </swiper>
